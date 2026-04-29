f22044285779c5d78475ba0deb7547ac8ea0e672
DELL <DELL@DESKTOP-QC7H7U7.home>
2026-04-30 02:20:14 +0300

    test

---
+++ src/main/java/cinemaSystem/Account.java
@@ -0,0 +1,37 @@
+package cinemaSystem;
+
+public class Account {
+	
+		private String name;
+		private String password;
+		private String Email;
+		private double balance;
+		
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getPassword() {
+			return password;
+		}
+		public void setPassword(String password) {
+			this.password = password;
+		}
+		public String getEmail() {
+			return Email;
+		}
+		public void setEmail(String email) {
+			Email = email;
+		}
+		public double getBalance() {
+			return balance;
+		}
+		public void setBalance(double balance) {
+			this.balance = balance;
+		}
+		
+
+	}
+}
